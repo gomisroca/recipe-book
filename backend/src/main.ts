427f08619d3cb6f6e4a67b471ece47aec9a2eefd
Adrià Gomis <gomisroca@gmail.com>
2026-02-14 12:06:10 +0100

    add global interceptor

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,5 +1,6 @@
-import { NestFactory } from '@nestjs/core';
+import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ClassSerializerInterceptor } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -8,6 +9,11 @@ async function bootstrap() {
     methods: ['GET', 'POST', 'DELETE', 'PUT', 'PATCH', 'OPTIONS'],
     credentials: true,
   });
+  app.useGlobalInterceptors(
+    new ClassSerializerInterceptor(app.get(Reflector), {
+      excludeExtraneousValues: true,
+    }),
+  );
   await app.listen(process.env.PORT ?? 3001);
 }
 void bootstrap();
