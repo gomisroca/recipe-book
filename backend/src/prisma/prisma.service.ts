2b8942fef777e4bc3f9ea1f5e6a03992bef917a9
Adrià Gomis <gomisroca@gmail.com>
2026-02-14 12:05:54 +0100

    restructure prisma

--- backend/src/prisma/prisma.service.ts
+++ backend/src/prisma/prisma.service.ts
@@ -1,7 +1,7 @@
 import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
 import 'dotenv/config';
 import { PrismaPg } from '@prisma/adapter-pg';
-import { PrismaClient } from '@generated/prisma/client';
+import { PrismaClient } from '@/generated/prisma/client';
 
 @Injectable()
 export class PrismaService
